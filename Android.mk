dec70586e371129b9c2030c070eaeb894691f0e7
Arne Coucheron <arco68@gmail.com>
2013-03-23 00:54:53 +0100

    Change makefiles for qcom legacy
    Change-Id: Ide4d145f406bc73231bb32d8c2454d3e794cad0a

--- Android.mk
+++ Android.mk
@@ -1,10 +1,5 @@
-QCOM_MEDIA_ROOT := $(call my-dir)
-ifneq ($(filter msm8974 msm8960 msm8660 msm7x27a msm7x30,$(TARGET_BOARD_PLATFORM)),)
-include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
-include $(QCOM_MEDIA_ROOT)/libstagefrighthw/Android.mk
-endif
+ifeq ($(TARGET_QCOM_DISPLAY_VARIANT),legacy)
 
-ifneq ($(filter msm8974 msm8960 msm8660 msm7x30,$(TARGET_BOARD_PLATFORM)),)
-include $(QCOM_MEDIA_ROOT)/mm-video/Android.mk
-include $(QCOM_MEDIA_ROOT)/libI420colorconvert/Android.mk
+include $(call all-subdir-makefiles)
+
 endif
